fe83276174d533fbea26aea83c195e7bc9cb0f9d
Jexica Ayran <jexica@jexicas-air.myfiosgateway.com>
2022-06-09 19:08:06 -0400

    setting up routes and creating screens/components

--- react-portfolio/client/src/App.js
+++ react-portfolio/client/src/App.js
@@ -1,9 +1,21 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Layout from "./Components/Layout/Layout";
+import Main from "./Screens/Main";
+import About from "./Screens/About";
+import Portfolio from "./Screens/Portfolio";
+import ProjectContainer from "./Components/ProjectContainer/ProjectContainer";
 
 function App() {
   return (
     <div className="App">
-      <div></div>
+      <Layout />
+      <Routes>
+        <Route path="/" exact element={<Main />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="/portfolio/:project" element={<ProjectContainer />} />
+      </Routes>
     </div>
   );
 }
