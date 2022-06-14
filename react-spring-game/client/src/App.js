26576bf962733ee6c11acf054ff212c8c91bb970
Jexica Ayran <jexica@jexicas-air.myfiosgateway.com>
2022-06-13 23:38:38 -0400

    first commit / setting up project

---
+++ react-spring-game/client/src/App.js
@@ -0,0 +1,19 @@
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Screens/Home";
+import About from "./Screens/About";
+import GameContainer from "./Screens/GameContainer";
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/game" element={<GameContainer />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
